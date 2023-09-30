1a5c88d8348c5b9186e41d7ad2a0bee7b723599b
VishuGoyal07 <vg07102002@gmail.com>
2023-09-30 12:04:29 +0530

    final commit

--- src/groups/GroupsListItem.js
+++ src/groups/GroupsListItem.js
@@ -5,12 +5,11 @@ export const GroupsListItem=({ group })=>{
     const requestToJoin=async()=>{
         await postWithCredentials(`http://localhost:8080/groups/${group.id}/request`)
         alert('Your request has been submitted');
-    }
-    
+    };
     return (
         <div className="list-item">
             <div className="list-item-data">
-                {/* <p>{group.name}</p> */}
+                <p>{group.name}</p>
                 <p>Owned by: {group.owner.fullName}</p>
                 <p>{group.members.length} members</p>
             </div>
--- src/groups/MyGroupsListItem.js
+++ src/groups/MyGroupsListItem.js
@@ -4,9 +4,9 @@ import {Link} from 'react-router-dom';
 export const MyGroupsListItem=({group })=>
     <div className="list-item">
         <div className="list-item-data">
-            {/* <Link to={`/groups/${group.id}`}>
+            <Link to={`/groups/${group.id}`}>
                <h3>{group.name}</h3>
-            </Link> */}
+            </Link>
             <p>Owned by: {group.owner.fullName}</p>
             <p>{group.members.length} members</p>
         </div>
